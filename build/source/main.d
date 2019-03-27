6bde4c28733e9c89496a3b2a625d4dcb5d8087dc
TheYoshiStory <AyusmanSaha@gmail.com>
2019-03-26 23:40:26 -0700

    code refactoring and test code for motor controller + motor setup

---
+++ build/source/main.d
@@ -0,0 +1,84 @@
+build/source/main.o: build/source/main.cpp \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/MKE18F16.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/core_cm4.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/cmsis_version.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/cmsis_compiler.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/cmsis_gcc.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/system_MKE18F16.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/board/board.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/board/clock_config.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_common.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/fsl_device_registers.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/MKE18F16.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/MKE18F16_features.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_clock.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_common.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/board/pin_mux.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/board/peripherals.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/utilities/fsl_debug_console.h \
+ build/source/vcu.h build/source/mc.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/lib/canlight.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/System/Service.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_flexcan.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/lib/adc.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_adc12.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_clock.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/lib/gpio.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_gpio.h \
+ /home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_port.h
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/MKE18F16.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/core_cm4.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/cmsis_version.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/cmsis_compiler.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/cmsis_gcc.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/system_MKE18F16.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/board/board.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/board/clock_config.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_common.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/fsl_device_registers.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/MKE18F16.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/CMSIS/MKE18F16_features.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_clock.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_common.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/board/pin_mux.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/board/peripherals.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/utilities/fsl_debug_console.h:
+
+build/source/vcu.h:
+
+build/source/mc.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/lib/canlight.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/System/Service.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_flexcan.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/lib/adc.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_adc12.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_clock.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/lib/gpio.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_gpio.h:
+
+/home/asaha/Documents/MCUXpresso/workspace/MKELibrary/drivers/fsl_port.h:
---
+++ build/source/vcu.d
@@ -0,0 +1,6 @@
+build/source/vcu.o: build/source/vcu.cpp build/source/vcu.h \
+ build/source/mc.h
+
+build/source/vcu.h:
+
+build/source/mc.h:
